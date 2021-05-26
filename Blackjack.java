eea4d71f563345bfa7dfe002ad2d5b8eac6ee8a9
alexng0328 <84466856+alexng0328@users.noreply.github.com>
2021-05-26 09:35:33 -0700

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -5,21 +5,23 @@ public class Blackjack {
     private Player [] players;
     private int amtOfPlayers;
     private Scanner ij = new Scanner(System.in);
+    Dealer_Class dealer;
 
     public void getPlayers(){
         System.out.println("How many players would you like to have? (1-4)");
         amtOfPlayers = ij.nextInt();
-        for(int i = 0; i < amtOfPlayers; i++){
+        Player [] playerList = new Player[amtOfPlayers];
+        for(int i = 0; i < amtOfPlayers-1; i++){
             Player player = new Player();
             players[i] = player;
         }
     }
 
-    public void getBets() {
+    /* public void getBets() {
         for (int i = 0; i < amtOfPlayers; i++) {
             if (players[i].getBank() > 0) {
                 System.out.print("How much do you want to bet " + players[i].getName() + (" (1-" + players[i].getBank()) + ")? ");
-                betValue = ij.nextInt();
+                bet = ij.nextInt();
                 players[i].setBet(g);
                 if(player[i].bet <= player[i].bank){
                     player[i].bank = player[i].bank - player[i].bet;
@@ -29,4 +31,13 @@ public class Blackjack {
             }
         }
     }
+
+     */
+    public void firstDeal(){
+        for(int i = 0; i < amtOfPlayers; i++){
+            players[i].addToHand(dealer.DealaCard());
+            players[i].addToHand(dealer.DealaCard());
+        }
+    }
+
 }
